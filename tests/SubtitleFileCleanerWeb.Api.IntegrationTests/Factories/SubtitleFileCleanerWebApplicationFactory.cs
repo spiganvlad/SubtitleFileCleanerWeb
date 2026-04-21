8c6bd82a8663de00819d78616150d26c5bd49095
spiganvlad <spiganvlad@gmail.com>
2026-04-21 09:20:17 +0300

    Setup test web application factory and initializers

---
+++ tests/SubtitleFileCleanerWeb.Api.IntegrationTests/Factories/SubtitleFileCleanerWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace SubtitleFileCleanerWeb.Api.IntegrationTests.Factories;
+
+public class SubtitleFileCleanerWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly List<ITestServiceInitializer> serviceInitializers = [];
+
+    public SubtitleFileCleanerWebApplicationFactory(params ITestServiceInitializer[] initializers)
+    {
+        foreach (var initializer in initializers)
+        {
+            ArgumentNullException.ThrowIfNull(initializer);
+            serviceInitializers.Add(initializer);
+        }
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        if (serviceInitializers.Count == 0)
+            return;
+
+        builder.ConfigureTestServices(services =>
+        {
+            foreach (var initializer in serviceInitializers)
+                initializer.Initialize(services);
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && serviceInitializers.Count > 0)
+        {
+            foreach (var initializer in serviceInitializers)
+                initializer.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+}
---
+++ tests/SubtitleFileCleanerWeb.Api.IntegrationTests/Helpers/Database/SqliteDatabaseInitializer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SubtitleFileCleanerWeb.Api.IntegrationTests.Factories;
+using SubtitleFileCleanerWeb.Infrastructure.Persistence;
+
+namespace SubtitleFileCleanerWeb.Api.IntegrationTests.Helpers.Database;
+
+public class SqliteDatabaseInitializer : ITestServiceInitializer
+{
+    private readonly string _connectionString = $"Data Source={Guid.NewGuid()}.sqlite;";
+    private readonly List<object> _seedData = [];
+    private ApplicationDbContext? _dbContext;
+    private bool _isDisposed;
+
+    public void Initialize(IServiceCollection services)
+    {
+        InitializeDatabase(services);
+
+        if (_seedData.Count > 0)
+            InitializeSeedData();
+    }
+
+    public void InitializeDatabase(IServiceCollection services)
+    {
+        services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+        services.AddSqlite<ApplicationDbContext>(_connectionString);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        dbContext!.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+
+        _dbContext = dbContext;
+    }
+
+    public void InitializeSeedData()
+    {
+        _dbContext!.AddRange(_seedData);
+        _dbContext.SaveChanges();
+    }
+
+    public void AddSeedData(object data) => _seedData.Add(data);
+
+    public void Dispose()
+    {
+        if (_isDisposed)
+            return;
+
+        _dbContext?.Database.EnsureDeleted();
+        _isDisposed = true;
+    }
+}
